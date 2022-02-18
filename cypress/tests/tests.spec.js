97f5ed11c38cacd774dde682b5fb3df360065528
Alex S <smolyakov.oleg.alex@gmail.com>
2022-02-18 10:46:58 -0600

    Canary (#6)
    * Activate registration test
    
    * Added API tests
    
    * Fixed fallen test
    
    * Fixed cod error
    
    * Deleted extra workflow
    
    * Skipped registration test
    
    * Added function in page_obgect
    
    * Added tsconfig.json
    
    * Added packcage-lock.json and modified package.json
    
    * Deleted tsconfig

--- cypress/tests/tests.spec.js
+++ cypress/tests/tests.spec.js
@@ -6,20 +6,29 @@ beforeEach(() => {
   cy.visit('/');
 })
 
+describe('API tests', ()=>{
+    it('the site is opening with status 200', () =>{
+      cy.request("https://vuejs-shopping-cart.coddeine.com/").then((result) => {
+        expect(result).to.have.property("status", 200)
+      })
+    })
+  })
+
+describe('Registration', () => {
+    it("tests duplicated email can't be registered", () => {
+      onLoginPage.registerNewUser();
+      cy.contains('The email address is already in use by another account.')
+        .should('be.visible');
+      })
+    })  
+
+
 describe('Login', () => {
     it('tests that user can login with valid credentials', () => {
       onLoginPage.login();
       cy.contains('Logout').should('be.visible');
-    })
-  })
-  
-describe('Registration', () => {
-  it("tests duplicated email can't be registered", () => {
-    onLoginPage.registerNewUser();
-    cy.contains('The email address is already in use by another account.')
-      .should('be.visible');
-    })
-  })
+      })
+    })  
 
 describe('Registration', () => {
     it("Tests duplicated email can't be registered", () => {
@@ -29,12 +38,21 @@ describe('Registration', () => {
         .should('be.visible');
       })
     })
-    
+   
 describe('Shopping cart/Checkout', () => {
     it('tests that product can be added to the cart', () => {
       onMainPage.addProductToCart("macbook Retina 13.3' ME662 (2013)");
       onMainPage.openCart();
       onCheckoutPage.checkProductIsInCart("macbook Retina 13.3' ME662 (2013)");
+    })
+
+    it('test that the shopping cart is cleared after user logged out', ()=>{
+      onLoginPage.login();
+      onMainPage.addProductToCart("macbook Retina 13.3' ME662 (2013)");
+      cy.contains('.btn', 'Checkout').should('contain', '1 ($ 2399)');
+      cy.wait(5000);
+      onLoginPage.logout();
+      cy.contains('.btn', 'Checkout').should('contain', '0 ($ 0)');
     })
 
     it('tests that multipple products can be added to the cart', () => {
@@ -61,7 +79,7 @@ describe('Shopping cart/Checkout', () => {
       cy.contains('Your order has been successfully processed!').should('be.visible');
     })
 
-    it.skip("tests that quantity of products can't be negative", () => {
+    it("tests that quantity of products can't be negative", () => {
       cy.log('This is a BUG and test is expected to fail');
       onLoginPage.login();
       onMainPage.addProductToCart("macbook Retina 13.3' ME662 (2013)");
